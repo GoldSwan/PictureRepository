ebc8b930cb889bd4fea2b9f69a8419279a630947
goldswan <saltmenu@naver.com>
2020-07-16 23:04:51 +0900

    Fix User model
    Fix User model using '@Column', '@NotEmpty','@Email' Annotation for 
    model validation and table column settings.

--- src/main/java/com/swan/picturerepository/model/User.java
+++ src/main/java/com/swan/picturerepository/model/User.java
@@ -1,9 +1,12 @@
 package com.swan.picturerepository.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,8 +20,18 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
-	private String userName;
+	@Column(length=20, nullable = false)
+	@NotEmpty(message = "아이디를 입력해주세요.")
+	private String username;
+	@Column(length=20, nullable = false)
+	@NotEmpty(message = "비밀번호를 입력해주세요.")
 	private String password;
+	@Column(length=100, nullable = false)
+	@Email
+	@NotEmpty(message = "이메일을 입력해주세요.")
+	private String email;
+	@Column(length=50, nullable = true)
 	private String authority;
+	@Column(nullable = true)
 	private int enabled;
 }
