6762df791d2efb364e449e9e63385b2668534831
goldswan <saltmenu@naver.com>
2020-12-31 22:20:55 +0900

    [FIX]google Gson을 통한 JAVA객체 -> JSON 변환, Model -> ModelAndView방식으로 수정

--- src/main/java/com/swan/picturerepository/controller/SearchController.java
+++ src/main/java/com/swan/picturerepository/controller/SearchController.java
@@ -8,10 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 import com.swan.picturerepository.model.UserFileInfo;
 import com.swan.picturerepository.service.FileSearchService;
 
@@ -20,22 +22,27 @@ public class SearchController {
 	
 	@Autowired private FileSearchService fileSearchService;
 	List<UserFileInfo> fileList = null;
-	Map<String, String> searchData = new HashMap<>();
+	Map<String, String> map = new HashMap<>();
+	
 	@RequestMapping(value = "/search", method = {RequestMethod.GET})
-	public String doJoin(HttpServletRequest req, Model model) {
-		
+	public ModelAndView doSearch(HttpServletRequest req) throws JsonProcessingException {	
+		ModelAndView mv = new ModelAndView();
 		String strSearch = req.getParameter("search");	
 		fileList = fileSearchService.getSearchFileList(strSearch);
 		
 		int index = 0;
-		for(UserFileInfo userFileInfo : fileList) {
-			searchData.put(Integer.toString(index), userFileInfo.getFileId());
+		for(UserFileInfo userFileInfo : fileList) {			
+			map.put("image"+Integer.toString(index), userFileInfo.getFileId());
 			index++;
 		}
 
-		model.addAttribute("searchData", searchData);
+		String searchData = new Gson().toJson(map);
+		System.out.println(searchData);
+		
+		mv.addObject("searchData",searchData);
+		mv.setViewName("home");
 		 
-		return "home";
+		return mv;
 	}
 
 }
