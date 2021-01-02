a7d68cdd237780540044446774ed9f440a8fa95b
goldswan <saltmenu@naver.com>
2021-01-02 19:15:49 +0900

    [ADD][FIX]썸네일 업로드 경로 추가, Service에서 생성하던 UUID를 Controller에서 생성하도록 수정

--- src/main/java/com/swan/picturerepository/controller/UploadController.java
+++ src/main/java/com/swan/picturerepository/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.swan.picturerepository.controller;
 
+import java.io.File;
+import java.util.UUID;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +18,20 @@ import com.swan.picturerepository.service.FileUploadService;
 @Controller
 public class UploadController {
 	
-	@Resource(name = "uploadPath")
-	private String uploadPath;
+	@Resource(name = "imageUploadPath")
+	private String imageUploadPath;
+	@Resource(name = "thumbnailUploadPath")
+	private String thumbnailUploadPath;
 	@Autowired FileUploadService fileUploadService;
 	
 	@RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
 	public String uploadForm(Model model, @RequestParam("username") String username, @RequestParam("file") MultipartFile file) throws Exception {
 		 
+		UUID uid = UUID.randomUUID();
 		String fileName = file.getOriginalFilename();
-		String fileId = fileUploadService.uploadFile(uploadPath, file.getBytes(), fileName);
-		
-		
+		String fileId = uid.toString() + fileName;
+		File imageFile = fileUploadService.uploadImageFile(imageUploadPath, fileId, file.getBytes(), fileName);
+		fileUploadService.uploadThumbnailFile(thumbnailUploadPath, fileId, imageFile, fileName);
 		fileUploadService.createFileData(username, fileId, fileName);
 		model.addAttribute("savedImageName", fileName);
 
