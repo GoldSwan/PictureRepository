aaff544a687e1fd5e049d5954ff34314f5dd79c1
goldswan <saltmenu@naver.com>
2021-01-02 19:01:24 +0900

    [ADD]썸네일 이미지 파일 업로드 로직 추가

--- src/main/java/com/swan/picturerepository/service/FileUploadService.java
+++ src/main/java/com/swan/picturerepository/service/FileUploadService.java
@@ -1,24 +1,33 @@
 package com.swan.picturerepository.service;
 
 import java.io.File;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import com.swan.picturerepository.dao.UserFileInfoDAO;
 
+import net.coobird.thumbnailator.Thumbnails;
+
 @Service
 public class FileUploadService {
+
+	@Autowired
+	UserFileInfoDAO userFileInfoDAO;
+	static final int THUMB_WIDTH = 346;
+	static final int THUMB_HEIGHT = 440;
 	
-	@Autowired UserFileInfoDAO userFileInfoDAO;
+	public File uploadImageFile(String imageUploadPath, String savedName, byte[] fileData, String fileName) throws Exception {
+		File imageFile = new File(imageUploadPath, savedName);
+		FileCopyUtils.copy(fileData, imageFile);
+		return imageFile;
+	}
 	
-	public String uploadFile(String uploadPath, byte[] fileData, String fileName) throws Exception {
-		UUID uid = UUID.randomUUID();
-		String savedName = uid.toString() + fileName;
-		File target = new File(uploadPath, savedName);
-		FileCopyUtils.copy(fileData, target);
-		return savedName;
+	public void uploadThumbnailFile(String uploadPath, String savedName, File imageFile, String fileName) throws Exception {
+		File imageThumbnailFile = new File(uploadPath, savedName);
+		if (imageFile.exists()) {
+			imageThumbnailFile.getParentFile().mkdirs();
+			Thumbnails.of(imageFile).size(THUMB_WIDTH, THUMB_HEIGHT).toFile(imageThumbnailFile);
+		}
 	}
 	
 	public void createFileData(String username, String fileId, String fileName) {
