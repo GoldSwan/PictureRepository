7d098add61ff5d604e5d8870511d041680893385
goldswan <saltmenu@naver.com>
2021-06-20 15:34:23 +0900

    [ADD]JPA를 이용한 게시판, 태그 관계정의 추가, Serializable 추가
    1. 태그 테이블이 게시판 테이블의 키를 외래키로 참조하도록 @ManyToOne 설정
    2.[JPA] Composite-id class must implement Serializable 에러 해결 위해
    Serializable 추가

--- src/main/java/com/swan/picturerepository/model/BulletinTag.java
+++ src/main/java/com/swan/picturerepository/model/BulletinTag.java
@@ -1,10 +1,16 @@
 package com.swan.picturerepository.model;
 
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +20,18 @@ import lombok.ToString;
 @Setter
 @ToString
 @Entity
-public class BulletinTag {
+public class BulletinTag implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	@Id
-	private Long bulletinId;
+	private Long bulletinId;	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String tagId;
+	private Long tagId;
 	@Column(length=10)
 	private String tagName;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name ="bulletinId")
+	private BulletinBoard bulletinBoard;	
 }
