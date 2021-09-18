962312f9970ea75da488f2e4d89ec1d5755356cb
goldswan <saltmenu@naver.com>
2021-09-18 23:48:31 +0900

    [ADD]Junit 테스트를 위한 설정 코드 추가

---
+++ src/test/java/com/swan/picturerepository/config/Configure.java
@@ -0,0 +1,24 @@
+package com.swan.picturerepository.config;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration
+(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/dao-context.xml"
+		     ,"file:src/main/webapp/WEB-INF/spring/appServlet/security-context.xml"
+		     ,"file:src/main/webapp/WEB-INF/spring/appServlet/service-context.xml"
+		     ,"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@TestPropertySource
+(locations = {"/jdbc.properties"})
+public class Configure {
+
+	@Autowired
+	public ApplicationContext ctx;
+}
