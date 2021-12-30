1df7e0e35b46651ce1ba78d027e19c886558bf50
goldswan <saltmenu@naver.com>
2021-12-30 21:59:53 +0900

    [FIX]해시태그 관련 모델 수정

---
+++ src/main/java/com/swan/picturerepository/model/HashTag.java
@@ -0,0 +1,23 @@
+package com.swan.picturerepository.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class HashTag {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long tagId;
+	@Column(length=10)
+	private String tagName;
+}
